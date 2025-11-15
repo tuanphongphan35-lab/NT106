75eab034afc7dac57606c76fd7e27fb18cc37908
Fantom156 <tuanphongphan35@gmail.com>
2025-11-15 10:37:18 +0700

    Delete SQL directory

--- SQL/dbo.Users.data.sql
+++
@@ -1,3 +0,0 @@
-SET IDENTITY_INSERT [dbo].[Users] ON
-INSERT INTO [dbo].[Users] ([UserID], [Username], [Email], [Password], [Avatar]) VALUES (1002, N'Phong12345', N'Phongphan0060@gmail.com', N'$2a$11$tLWIJ9KxbNVCumC1N6ZWsueywEZHE9t5o3YbThn5l4l.0iR5lQZ7O', <Binary Data>)
-SET IDENTITY_INSERT [dbo].[Users] OFF
